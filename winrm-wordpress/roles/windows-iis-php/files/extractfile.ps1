a26365ee4578e15028fa099d311f90b0e8c5322d
Trond Hindenes <trond@hindenes.com>
2014-07-20 00:02:49 +0200

    mm

--- winrm-wordpress/roles/windows-iis-php/files/extractfile.ps1
+++ winrm-wordpress/roles/windows-iis-php/files/extractfile.ps1
@@ -1,3 +1,6 @@
-param ($Infile,$outfolder)
+#param ($Infile,$outfolder)
+
+$infile = "C:\php.zip"
+$outfolder = "C:\php"
 [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
 [System.IO.Compression.ZipFile]::ExtractToDirectory($Infile,$outfolder)
